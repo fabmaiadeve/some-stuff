8f3f1169ca40f45fe265694c82dd41a82f576ecf
FabianoMaia <fmaiati@gmail.com>
2021-08-03 20:44:28 -0300

    4 Formas de fazer Output no javaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+// 4 Formas principais de Output
+
+document.getElementById("titulo").innerHTML = "Olá, td certo!";
+
+document.write("Tenho algo mais aqui.");
+
+/* Alterando dados no browser atraves de uma janela do navegador*/
+window.alert("Texto de Alerta!"); // pode ser ==> alert("Texto de Alerta!");
+
+console.log("Isso vai aparecer no log!");
