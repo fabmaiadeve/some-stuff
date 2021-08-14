7457c4aa8c52d05974b82e160c6442e29ff80f30
FabianoMaia <fmaiati@gmail.com>
2021-08-14 18:13:51 -0300

    Trabalhando com switch e estruturas de repeticao

--- JavaScript/condicionais.js
+++ JavaScript/condicionais.js
@@ -38,3 +38,30 @@ if (op == '+') {
 } else {
 	console.error('Operation not found!');
 }
+
+// trabalhando com switch
+
+var num3 = prompt('Numero 3: ');
+var num4 = prompt('Numero 4: ');
+var op2 = prompt('operation: ');
+
+num3 = parseFloat(num3); 
+num4 = parseFloat(num4);
+
+switch (op2) {
+	case '+':
+		console.log(num3 + num4);
+	break;
+	case '-':
+		console.log(num3 - num4);
+	break;
+	case '*':
+		console.log(num3 * num4);
+	break;
+	case '/':
+		console.log(num3 / num4);
+	break;
+	default :
+		console.error('Erro');
+}
+
---
+++ JavaScript/repetition.js
@@ -0,0 +1,10 @@
+var i = 0;
+
+while (i < 5) {
+	console.log("Inside Loop");
+	i++;
+}
+
+for (var m = 0; m < 5; m++) {
+	console.log(m);
+}
