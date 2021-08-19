554f88c4599271f7279b3e5f93fc3a12a2fb0e6a
FabianoMaia <fmaiati@gmail.com>
2021-08-18 22:44:06 -0300

    Introducao sobre Funcoes

---
+++ JavaScript/funcao.js
@@ -0,0 +1,22 @@
+function sayHello() {
+	console.log('Hello!')
+}
+
+sayHello();
+/*
+var number = 4;
+
+function square() {
+	return number * number;
+}
+
+var result = square();
+*/
+
+function square(num) {
+	return num * num;
+} 
+
+var res = square(5);
+
+console.log(res);
