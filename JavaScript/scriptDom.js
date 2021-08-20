f7386442f354835b001a63807508e1c36d6ee26f
FabianoMaia <fmaiati@gmail.com>
2021-08-19 23:45:49 -0300

    Introducao DOM - Document Object Model - Selecionando Elementos

---
+++ JavaScript/scriptDom.js
@@ -0,0 +1,7 @@
+document.getElementById('exemplo').innerHTML = "Fabiano";
+
+document.getElementsByClassName('lista')[0].innerHTML = 'Item alterado na lista';
+
+document.getElementsByTagName('button');
+
+document.getElementsByName('email');
