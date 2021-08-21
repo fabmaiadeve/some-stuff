821c62c4fb23e433939fc47f9aa214953fb3e299
FabianoMaia <fmaiati@gmail.com>
2021-08-20 23:34:38 -0300

    DOM - usando querySelector

--- JavaScript/scriptDom.js
+++ JavaScript/scriptDom.js
@@ -5,3 +5,11 @@ document.getElementsByClassName('lista')[0].innerHTML = 'Item alterado na lista'
 document.getElementsByTagName('button');
 
 document.getElementsByName('email');
+
+document.querySelector('#exemplo');
+
+document.querySelector('.lista');
+
+document.querySelectorAll('.lista');
+
+document.querySelector('button').innerHTML = 'Mudando o botão!';
