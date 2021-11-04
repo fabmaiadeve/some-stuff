6fe539c91dfe0ded0789bb94f74654da4ba5e2b9
FabianoMaia <fmaiati@gmail.com>
2021-11-04 17:04:20 -0300

    manipulando strings

---
+++ JavaScript/manipulandoString.js
@@ -0,0 +1,26 @@
+let nome = 'Fabiano de Oliveira Maia';
+
+let hobby = ' dançar ';
+//let resultado = nome.length;
+
+//let resultado = nome.indexOf('Silva');
+
+//let resultado = nome.slice(-4);
+
+//let resultado = nome.substring(0, 10);
+
+//let resultado = nome.substr(-4, 4);
+
+//let resultado = nome.replace('Maia', 'Silva');
+
+//let resultado = nome.toUpperCase();
+
+//let resultado = nome.toLowerCase();
+
+//let resultado = hobby.trim();
+
+//let resultado = nome.charAt(5);
+
+let resultado = nome.split(' ');
+
+console.log(resultado);
